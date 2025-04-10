7d037840302c65dddd4715709e94df2a8031707a
Tejas Gadi <tejasgadi@Tejass-MacBook-Pro.local>
2025-04-10 17:31:02 +0530

    Removed Meeting from Sidebar

--- src/app/(protected)/AppSidebar.tsx
+++ src/app/(protected)/AppSidebar.tsx
@@ -22,11 +22,6 @@ const items =[
     icon: Bot
   },
   {
-    title: "Meetings",
-    url: "/meetings",
-    icon: Presentation,
-  },
-  {
     title: "Billing",
     url: "/billing",
     icon: CreditCard,
